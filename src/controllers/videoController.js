b8b8a06b4fa4d990b039c6c7700a3df4fe179ae9
JYoungKweon <48717976+JYoungKweon@users.noreply.github.com>
2021-06-26 23:25:50 +0900

    GET_POST_DONE
    Upload video with fake DB(array)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,6 +30,7 @@ export const trending = (req, res) => {
 };
 export const watch = (req, res) => {
   const { id } = req.params;
+  //fakeDB
   const video = videos[id - 1];
   return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
   //const id = req.params.id;
@@ -38,7 +39,31 @@ export const watch = (req, res) => {
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
+  //fakeDB
   const video = videos[id - 1];
   return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  //fakeDB
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  // here we will add a video to the videos array.
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
