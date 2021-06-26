08f2a63e83e92440b803a8c9b2ef5ac4c3ee892b
JYoungKweon <48717976+JYoungKweon@users.noreply.github.com>
2021-06-26 22:36:13 +0900

    get and post

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,39 +1,44 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 4,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 4,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 4,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 4,
-      comments: 2,
-      createdAt: "2minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 4,
-      comments: 2,
-      createdAt: "2minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 4,
-      comments: 2,
-      createdAt: "2minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-//console.log(req.params);
-
-export const search = (req, res) => res.send("Search Videos");
-export const upload = (req, res) => res.send("upload Video");
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send(`delete Video #${req.params.id}`);
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  //const id = req.params.id;
+  //console.log("Show video", id);
 };
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {};
