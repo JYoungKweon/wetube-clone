db99325ed8a201f257cbd9e10b3662448aa9566a
JYoungKweon <48717976+JYoungKweon@users.noreply.github.com>
2021-06-26 16:52:10 +0900

    Pug with param

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
-export const trending = (req, res) => res.send("Home Page");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send(`Edit #${req.params.id}`);
-};
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+//console.log(req.params);
+
 export const search = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("upload Video");
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -8,8 +8,11 @@ import videoRouter from "./routers/videoRouter";
 const PORT = 4040;
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("x-powered-by", false);
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
