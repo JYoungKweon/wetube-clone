a103b9441d8bfedfde13b386e2363af34990ebc4
JYoungKweon <48717976+JYoungKweon@users.noreply.github.com>
2021-06-26 15:39:28 +0900

    urlParams

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,15 @@
 export const trending = (req, res) => res.send("Home Page");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => {
+  console.log(req.params);
+  return res.send(`Watch #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  console.log(req.params);
+  return res.send(`Edit #${req.params.id}`);
+};
+export const search = (req, res) => res.send("Search Videos");
+export const upload = (req, res) => res.send("upload Video");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  return res.send(`delete Video #${req.params.id}`);
+};
