5966207c3ac9e82dd8742ec355b066227a4ca629
JYoungKweon <48717976+JYoungKweon@users.noreply.github.com>
2021-06-26 12:54:17 +0900

    about Middleware
    add protect, return

--- src/server.js
+++ src/server.js
@@ -3,14 +3,38 @@ import express from "express";
 const PORT = 4040;
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  //return res.send("lalalal");
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed");
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("I still love you.");
+  console.log("I'm in the handleHome!");
+  return res.send("I love Middleware.");
 };
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to private lounge!");
 };
+//const handleLogin = (req, res) => {
+//  return res.end({ message: "Login here." });
+//};
+
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
+//app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
