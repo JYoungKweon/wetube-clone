ad353259ecab6aeae48a22cd265630656157fa5e
JYoungKweon <48717976+JYoungKweon@users.noreply.github.com>
2021-06-27 09:30:32 +0900

    DB
    connect MongoDB, async & await

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 import express from "express";
 import morgan from "morgan";
-
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4040;
 const app = express();
 const logger = morgan("dev");
 
@@ -18,16 +16,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-const home = (req, res) => {
-  console.log("last return");
-  return res.send("hello World");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
-const handleListening = () =>
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
