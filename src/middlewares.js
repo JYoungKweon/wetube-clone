7f6a6a780d67fa89a8938ebca1c7843467557e75
JYoungKweon <kouhej2007@gmail.com>
2021-07-08 16:50:35 +0900

    Connect multerS3 with AWS S3

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,18 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
+const s3 = new aws.s3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: wetube - jyk,
+});
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
@@ -26,8 +39,10 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars/",
   limits: { fileSize: 3000000 },
+  storage: multerUploader,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: { fileSize: 100000000 },
+  storage: multerUploader,
 });
